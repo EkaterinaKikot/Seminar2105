aeb9f7145fc74b036b4a8aa365734687188a926a
Kikot Ekaterina <kati-kik@yandex.ru>
2023-05-23 13:09:44 +0300

    решена 13 задача

---
+++ Task13/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+while (n < 100 || n > 999)
+{
+    Console.Write("Третьей цифры нет! Введите число: ");
+n = int.Parse(Console.ReadLine()!);
+}
+Console.WriteLine(n % 10);
