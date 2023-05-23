7a929769f3a401ccd85e7b7ce40f0ebb2b92f00f
Kikot Ekaterina <kati-kik@yandex.ru>
2023-05-23 13:31:47 +0300

    решена 15 задача

---
+++ Task15/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+while (n < 1 || n > 7)
+{
+    Console.Write("Вы ошиблись! Введите номер дня недели: ");
+    n = int.Parse(Console.ReadLine()!);
+}
+if (n == 1)
+Console.WriteLine("Нет");
+else if (n == 2)
+Console.WriteLine("Нет");
+else if (n == 3)
+Console.WriteLine("Нет");
+else if (n == 4)
+Console.WriteLine("Нет");
+else if (n == 5)
+Console.WriteLine("Нет");
+else if (n == 6)
+Console.WriteLine("Да");
+else if (n == 7)
+Console.WriteLine("Да");
+
+
