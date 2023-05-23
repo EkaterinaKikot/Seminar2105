53f4ab88c00504f2cb0a5993f97c528881de33cc
Kikot Ekaterina <kati-kik@yandex.ru>
2023-05-23 12:55:57 +0300

    решена 10 задача

---
+++ Task10/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+while (n < 100 || n > 999)
+{
+    Console.Write("Введите число: ");
+    n = int.Parse(Console.ReadLine()!);
+}
+Console.WriteLine(n = (n / 10) % 10) ;
